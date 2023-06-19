620c05d662f5af83d6e060d9dfa1f41fc1dcfedc
Louis Klein <louisklein71@gmail.com>
2023-06-18 21:28:53 -0700

    test

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,32 @@
 class Song:
-    pass
+    all = []
+    count = len(all)
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_to_all(self, genre, artist)
+
+    @classmethod
+    def add_to_all(cls, song=None, genre=None, artist = None):
+        if song is not None:
+            cls.all.append(song)
+        if genre is not None:
+            cls.genres.append(genre)
+            cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
+        if artist is not None:
+            cls.artists.append(artist)
+            cls.artist_count[artist] = cls.artist_count.get(artist,0) +1
+        cls.count = len(Song.all)
+
+    # def add_to_all(cls, song=None, genre=None):
+    # if song is not None:
+    #     cls.all.append(song)
+    #     cls.count = len(Song.all)
+    # if genre is not None:
+    #     cls.genres.append(genre)
